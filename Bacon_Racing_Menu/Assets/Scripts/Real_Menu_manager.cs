06810ed9362c5f106c308fceac48dd5685f143a4
ALD1YO <crespoaldo29@gmail.com>
2019-01-31 05:10:35 -0600

    Se empieza la programación de la interfaz del juego, se logra implementar placeholder para música y demás placeholders de imágenes. Se logra navegar exitosamente hasta la selección de pista

---
+++ Bacon_Racing_Menu/Assets/Scripts/Real_Menu_manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Real_Menu_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Main_Menu_Canvas;
+    public GameObject Character_Screen;
+    public GameObject Tracks_Screen;
+    public void To_Title_Screen(){
+        Main_Menu_Canvas.SetActive(true);
+        Character_Screen.SetActive(false);
+        Tracks_Screen.SetActive(false);
+    }
+
+    public void To_Characters_Screen(){
+         Main_Menu_Canvas.SetActive(false);
+        Character_Screen.SetActive(true);
+        Tracks_Screen.SetActive(false);
+    }
+
+     public void To_Tracks_Screen(){
+        Main_Menu_Canvas.SetActive(false);
+        Character_Screen.SetActive(false);
+        Tracks_Screen.SetActive(true);
+    }
+}
