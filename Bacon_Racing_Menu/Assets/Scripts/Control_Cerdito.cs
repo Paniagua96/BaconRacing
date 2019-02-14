7a5a9829b175424c1e90de457cef4960e8de1d61
Juan-Casanova <casanovajm199621@gmail.com>
2019-02-13 21:50:09 -0600

    Control_Cerdo-Wayponits_CerdoEnemigo-Velocimetro

---
+++ Bacon_Racing_Menu/Assets/Scripts/Control_Cerdito.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control_Cerdito : MonoBehaviour
+{
+    public Text TextSpeed;
+    public WheelCollider Front_Left;
+    public WheelCollider Front_Right;
+    public WheelCollider Back_Left;
+    public WheelCollider Back_Right;
+    public float Torque;
+    public float Speed;
+    public float MaxSpeed=200f;
+    public int Brake=10000;
+    public float CoefAccelaration=10f;
+    public float WheelAngleMax=5;
+    public Rigidbody Cerdito;
+    //public object Cerdito2;
+
+    void Start()
+    {
+        Cerdito.centerOfMass=new Vector3(0,.3f,0);
+        //Cerdito2.centerOfMass=new Vector3(0,-1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Girar=Input.GetAxis("Horizontal");
+
+        //VISUALIZATION OF SPEED
+        Speed=GetComponent<Rigidbody>().velocity.magnitude*3.6f;
+        TextSpeed.text ="Velocidad: "+(int)Speed+" KM/HR";
+
+        //ACCERELATION
+        if(Input.GetKey(KeyCode.UpArrow) && Speed<MaxSpeed)
+        {
+            Back_Left.brakeTorque=0;
+            Back_Right.brakeTorque=0;
+            Back_Left.motorTorque=Input.GetAxis("Vertical")*Torque*CoefAccelaration*Time.deltaTime;
+            Back_Right.motorTorque=Input.GetAxis("Vertical")*Torque*CoefAccelaration*Time.deltaTime;
+            Front_Left.brakeTorque=0;
+            Front_Right.brakeTorque=0;
+            Front_Left.motorTorque=Input.GetAxis("Vertical")*Torque*CoefAccelaration*Time.deltaTime;
+            Front_Right.motorTorque=Input.GetAxis("Vertical")*Torque*CoefAccelaration*Time.deltaTime;
+        }
+
+        //DECELERATION
+        if(!Input.GetKey(KeyCode.UpArrow) || Speed>MaxSpeed)
+        {
+            Back_Left.brakeTorque=0;
+            Back_Right.brakeTorque=0;
+            Back_Left.brakeTorque=Brake*CoefAccelaration*Time.deltaTime;
+            Back_Right.brakeTorque=Brake*CoefAccelaration*Time.deltaTime;
+            Front_Left.brakeTorque=0;
+            Front_Right.brakeTorque=0;
+            Front_Left.brakeTorque=Brake*CoefAccelaration*Time.deltaTime;
+            Front_Right.brakeTorque=Brake*CoefAccelaration*Time.deltaTime;
+        }
+
+        //Direction of the pig
+        Front_Left.steerAngle=Input.GetAxis("Horizontal")*WheelAngleMax;
+        Front_Right.steerAngle=Input.GetAxis("Horizontal")*WheelAngleMax;
+        
+        if(Speed>2)
+        {
+            if(Girar<0){
+                Cerdito.transform.Rotate(0,-2f,0);
+                
+            }
+
+            if(Girar>0){
+                Cerdito.transform.Rotate(0,2f,0);
+            }
+        }
+        //if(Girar==0){
+            //Cerdito.transform.Rotate(0,0,0);
+        //}
+        Debug.Log(Girar);
+    }
+}
