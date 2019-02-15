0e2f8273ae2c0c4a324813439dce2ba355b166a2
Paniagua96 <paniagua-11@hotmail.com>
2019-02-14 19:45:39 -0600

    Se agrego boton para restar escena

---
+++ Bacon_Racing_Menu/Assets/Scripts/BarrilRespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrilRespawn : MonoBehaviour
+{
+ 
+   
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+        StartCoroutine(DisapearBarrilForSeconds());
+       
+    }
+
+    public IEnumerator DisapearBarrilForSeconds()
+    {
+       this.transform.position=new Vector3(this.transform.position.x,this.transform.position.y-10);
+        yield return new WaitForSeconds(3);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y +10);
+
+    }
+
+    
+}
